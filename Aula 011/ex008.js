060a944e8e793fd2ff9cb5d1c823dd8fcec7ce36
Barbara Viana <81521722+BarbaraViana@users.noreply.github.com>
2022-11-14 15:00:57 -0300

    creat ex008.js

---
+++ Aula 011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel} por hora`)
+if (vel > 60) { //condição simples
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log(`dirija com segurança`)
